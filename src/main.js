d3384ebc1a0a88a38e3fabdc236965228ded834f
sd <3274381577>
2020-10-27 11:26:20 +0800

    引入vant

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './styles/index.less'
+// 引入vant
+import Vant from 'vant'
+// 引入vant的css
+import 'vant/lib/index.css'
+// 全局挂载
+Vue.use(Vant)
+// 阻止启动生产消息
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
